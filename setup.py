1d787385d5bfd232a9f1055a6f2d67704f2c2084
abalogh2 <adam.balogh2@emarsys.com>
2017-08-08 15:35:03 +0200

    Create setup.py script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ems-gcp-tools',
+    version='',
+    packages=['tests', 'gcp_tools'],
+    url='',
+    license='',
+    author='ems',
+    author_email='',
+    description=''
+)
